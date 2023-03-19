447bdfed879de36bc3f0a30a94fa1aaa50f9a25e
baraka <48273892+Wekesabaraka@users.noreply.github.com>
2023-03-19 05:57:10 -0700

    6-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Program propergrammar
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int a;
-	long int b;
+	int m;
+	long int n;
 	long long int c;
-	char d;
+	char k;
 	float f;
 
-	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
-	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(k));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(m));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(n));
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
