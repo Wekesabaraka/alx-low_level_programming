2b514882770c16ca6ace482e0870308eb8a32d99
baraka <48273892+Wekesabaraka@users.noreply.github.com>
2023-03-20 12:59:37 -0700

    6print numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int k;
+
+	for (k = '0'; k <= '9'; k++)
+		putchar(d);
+	putchar('\n');
+
+	return (0);
+}
