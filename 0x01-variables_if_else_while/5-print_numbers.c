1042816b17b9a2528df9905211ced4e8c77a7912
baraka <48273892+Wekesabaraka@users.noreply.github.com>
2023-03-20 12:44:01 -0700

    5print

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int m;
+
+	for (m = 0; m < 10; m++)
+		printf("%d", m);
+	printf("\n");
+
+	return (0);
+}
